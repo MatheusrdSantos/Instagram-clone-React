f761377242b11fe9a59bcb5e6b6069848a02df93
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-12 18:42:17 -0300

    Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header/>*/}
+      <Header/>
       <Routes></Routes>
     </BrowserRouter>
   );
