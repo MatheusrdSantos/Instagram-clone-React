56f18dc25195779aa31a7cb08e6ab805005f0d40
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-12 19:21:28 -0300

    Post layout created

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,10 +1,42 @@
-
 import React, {Component} from 'react';
-
+import './Feed.css';
+import api from '../services/api';
+import more from '../assets/more.svg';
+import send from '../assets/send.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
 class Feed extends Component{
+    state = {
+        feed: []
+    }
+
+    async componentDidMount(){
+        const response = await api.get('posts');
+        this.setState({feed: response.data});
+    }
     render(){
         return (
-         <h1>Feed</h1>   
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Matheus Santos</span>
+                            <span className="place">Natal, Brazil</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    <img src="http://localhost:3333/files/my_profile.jpg" alt=""/>
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="Like"/>
+                            <img src={comment} alt="Comment"/>
+                            <img src={send} alt="Send"/>
+                        </div>
+                        <strong>10 curtidas</strong>
+                        <p>Minha nova foto de perfil<span>#novafoto</span></p>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
